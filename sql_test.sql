8f8cc92ebae768754618b897abe4eb1fdb3d8de3
kayeehan <hkmhcm@gmail.com>
2022-02-03 18:04:21 +0900

    02/03 commit

--- sql_test.sql
+++ sql_test.sql
@@ -105,3 +105,57 @@ WHERE employee_id in (SELECT employee_id
                 FROM(SELECT EMPLOYEE_ID, rank() over(partition by DEPARTMENT_ID order by SALARY desc) ord_sal
                  FROM employees)
                  WHERE ord_sal=1);
+
+--동물 보호소에 들어온 모든 동물의 정보를 ANIMAL_ID순으로 조회하는 SQL문을 작성해주세요. 
+SELECT *
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
+
+--동물 보호소에 들어온 모든 동물의 이름과 보호 시작일을 조회하는 SQL문을 작성해주세요. 이때 결과는 ANIMAL_ID 역순으로 보여주세요. 
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID desc;
+
+--동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY 1;
+
+--동물 보호소에 들어온 동물 중 젊은 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION NOT IN ('Aged')
+ORDER BY 1;
+
+--동물 보호소에 들어온 모든 동물의 아이디와 이름을 ANIMAL_ID순으로 조회하는 SQL문을 작성해주세요.
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+ORDER BY 1;
+
+--동물 보호소에 들어온 모든 동물의 아이디와 이름, 보호 시작일을 이름 순으로 조회하는 SQL문을 작성해주세요. 단, 이름이 같은 동물 중에서는 보호를 나중에 시작한 동물을 먼저 보여줘야 합니다.
+SELECT ANIMAL_ID,NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME, DATETIME DESC;
+
+--동물 보호소에 가장 먼저 들어온 동물의 이름을 조회하는 SQL 문을 작성해주세요.
+SELECT NAME
+FROM ANIMAL_INS
+WHERE DATETIME= (SELECT MIN(DATETIME)
+                FROM ANIMAL_INS)
+
+--가장 최근에 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+SELECT MAX(DATETIME)
+FROM ANIMAL_INS;
+
+--동물 보호소에 가장 먼저 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+SELECT MIN(DATETIME)
+FROM ANIMAL_INS;
+
+--동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성해주세요.
+SELECT COUNT(*)
+FROM ANIMAL_INS;
+
+--동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL 문을 작성해주세요. 이때 이름이 NULL인 경우는 집계하지 않으며 중복되는 이름은 하나로 칩니다.
+SELECT COUNT(DISTINCT NAME)
+FROM ANIMAL_INS;
