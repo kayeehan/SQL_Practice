695fdebb1f0b39adbf46ba15ffd3501f997ca07a
kayeehan <hkmhcm@gmail.com>
2022-02-05 22:23:23 +0900

    02/05 commit

--- sql_test.sql
+++ sql_test.sql
@@ -246,3 +246,37 @@ AND EXISTS (SELECT 'X'
                  WHERE SEX_UPON_INTAKE in ('Intact Male','Intact Female')
                  AND O.ANIMAL_ID=ANIMAL_ID)
 ORDER BY 1;
+
+--동물 보호소에 들어온 동물 중 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty인 동물의 아이디와 이름, 성별 및 중성화 여부를 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요. 
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
+ORDER BY 1;
+
+--보호소에 돌아가신 할머니가 기르던 개를 찾는 사람이 찾아왔습니다. 이 사람이 말하길 할머니가 기르던 개는 이름에 'el'이 들어간다고 합니다. 동물 보호소에 들어온 동물 이름 중, 이름에 "EL"이 들어가는 개의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 이때 결과는 이름 순으로 조회해주세요. 단, 이름의 대소문자는 구분하지 않습니다.
+SELECT animal_id, name
+FROM ANIMAL_INS
+WHERE animal_type = 'Dog'
+AND lower(name) like '%el%'
+ORDER BY 2;
+
+--보호소의 동물이 중성화되었는지 아닌지 파악하려 합니다. 중성화된 동물은 SEX_UPON_INTAKE 컬럼에 'Neutered' 또는 'Spayed'라는 단어가 들어있습니다. 동물의 아이디와 이름, 중성화 여부를 아이디 순으로 조회하는 SQL문을 작성해주세요. 이때 중성화가 되어있다면 'O', 아니라면 'X'라고 표시해주세요.
+SELECT animal_id, name,
+        (CASE
+         when regexp_like(sex_upon_intake,'Neutered.*|Spayed') then 'O'
+         else 'X' end) as 중성화
+FROM ANIMAL_INS
+ORDER BY 1;
+
+--입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 이때 결과는 보호 기간이 긴 순으로 조회해야 합니다.
+SELECT animal_id, name
+FROM (SELECT O.animal_id, O.name, O.datetime-I.datetime
+        FROM ANIMAL_OUTS O, ANIMAL_INS I
+        WHERE I.animal_id = O.animal_id
+        ORDER BY 3 desc)
+WHERE rownum <= 2;
+
+--ANIMAL_INS 테이블에 등록된 모든 레코드에 대해, 각 동물의 아이디와 이름, 들어온 날짜1를 조회하는 SQL문을 작성해주세요. 이때 결과는 아이디 순으로 조회해야 합니다.
+SELECT animal_id, name, to_char(datetime,'YYYY-MM-DD') 날짜
+FROM ANIMAL_INS
+ORDER BY 1;
