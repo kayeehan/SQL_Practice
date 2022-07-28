ea2194178dee077d36ada589fc4acdbf0e24fbf1
Kyeongmin Han <98004520+kayeehan@users.noreply.github.com>
2022-07-28 19:29:48 +0900

    7/28 commit

--- sql_test.sql
+++ sql_test.sql
@@ -855,3 +855,49 @@ SELECT actor_id, director_id
 FROM ActorDirector
 GROUP BY actor_id, director_id
 HAVING count(timestamp) >= 3;
+
+/*626. Exchange Seats
+Write an SQL query to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+Return the result table ordered by id in ascending order.
+The query result format is in the following example.
+
+Input: 
+Seat table:
++----+---------+
+| id | student |
++----+---------+
+| 1  | Abbot   |
+| 2  | Doris   |
+| 3  | Emerson |
+| 4  | Green   |
+| 5  | Jeames  |
++----+---------+
+Output: 
++----+---------+
+| id | student |
++----+---------+
+| 1  | Doris   |
+| 2  | Abbot   |
+| 3  | Green   |
+| 4  | Emerson |
+| 5  | Jeames  |
++----+---------+
+Explanation: 
+Note that if the number of students is odd, there is no need to change the last one's seat.
+*/
+SELECT id "id", name "student"
+FROM (SELECT id, lag(student,1) over(order by student) name
+        FROM seat)
+WHERE mod(id,2) = 0
+AND name is not null
+UNION
+SELECT id, name
+FROM (SELECT id, lead(student,1) over(order by student) name
+        FROM seat)
+WHERE mod(id,2) = 1
+AND name is not null
+UNION
+SELECT id, student 
+FROM seat
+WHERE id = (SELECT max(id)
+           FROM seat);
